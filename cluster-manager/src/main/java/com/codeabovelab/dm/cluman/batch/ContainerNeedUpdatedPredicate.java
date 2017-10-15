7dd1e4f90e3c2006ab87785acf6e9837ab7e96e9
pronto <vitaly.pronto@gmail.com>
2017-10-15 19:55:07 +0300

    Improved logging

--- cluster-manager/src/main/java/com/codeabovelab/dm/cluman/batch/ContainerNeedUpdatedPredicate.java
+++ cluster-manager/src/main/java/com/codeabovelab/dm/cluman/batch/ContainerNeedUpdatedPredicate.java
@@ -77,7 +77,7 @@ public class ContainerNeedUpdatedPredicate implements ContainerPredicate {
     private boolean exclude(ProcessedContainer pc) {
         ImagesForUpdate.Exclude exclude = images.getExclude();
         Set<String> containers = exclude.getContainers();
-        log.info("Excluded: {}, processedContainer: {}", pc);
+        log.info("Checking containers, list of excluded: {}, processedContainer: {}", containers, pc);
         return containers.contains(pc.getId()) ||
           containers.contains(pc.getName()) ||
           exclude.getNodes().contains(pc.getNode());
